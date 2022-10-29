82356351384288bc4b35ce1bdec7c55af967b3d6
Klithik <santiago.silva@mayor.cl>
2022-10-29 11:43:37 -0300

    agregado bucle para acciones consecutivas

--- main.py
+++ main.py
@@ -21,12 +21,24 @@ print('Bienvenido a Pikachu 3. ¿Que accion quieres realizar?')
 print('[1] Ordenar archivos por fecha y/o tipo')
 print('[2] Buscar un archivo o directorio')
 print('[3] Sacar todos los archivos de una carpeta')
+print('[4] Salir')
 eleccion = input()
 revisaRespuesta(eleccion)
 
-if eleccion == '1':
-    agrupa.pregunta()
-elif eleccion == '2':
-    busca.inicia()
-elif eleccion == '3':
-    separa.inicia()
+while(True):
+    if eleccion == '1':
+        agrupa.main()
+    elif eleccion == '2':
+        busca.inicia()
+    elif eleccion == '3':
+        separa.inicia()
+    elif eleccion == '4':
+        exit()
+    
+    print('¿Quieres hacer algo mas?')
+    print('[1] Ordenar archivos por fecha y/o tipo')
+    print('[2] Buscar un archivo o directorio')
+    print('[3] Sacar todos los archivos de una carpeta')
+    print('[4] Salir')
+    eleccion = input()
+    revisaRespuesta(eleccion)
